93767661aced39721c5fc4485efef8f3e765ae4c
Luzifer197 <fasanellamassimo16@gmail.com>
2023-10-26 20:05:05 +0200

    delete unsplash api key, add it as variable

--- selfFunction.py
+++ selfFunction.py
@@ -1,8 +1,8 @@
 import requests
 
-def get_random_image_url():
+def get_random_image_url(api_unsplash):
     # Setzen Sie Ihren Unsplash API-Schlüssel hier ein
-    unsplash_api_key = "6-ElB1ia3oWxolxyx5JGSxnJPVJv_zLVaz0moIOuWRs"
+    unsplash_api_key = api_unsplash
     # Rufen Sie ein zufälliges Bild von Unsplash ab
     response = requests.get("https://api.unsplash.com/photos/random", headers={"Authorization": f"Client-ID {unsplash_api_key}"})
     if response.status_code == 200:
